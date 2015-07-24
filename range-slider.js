18cff8b1cd18e64e8a0a373cb3b81ff868766461
Ankit Gupta <aga53@sfu.ca>
2015-07-23 18:33:24 -0700

    added library

---
+++ range-slider.js
@@ -0,0 +1,151 @@
+function RangeSlider(id, params){
+    "use strict"
+
+    var sliderObj = this;
+
+    if( !(this instanceof RangeSlider) ){
+        throw new Error("RangeSlider needs to be called with the new keyword");
+    }
+
+    this.id = id;
+
+    if (params === undefined) {
+        params = {};
+    }
+
+    this.margin = {top: 0, left: 10, right: 15, bottom: 2};
+
+    var min = params.min === undefined ? 0 : params.min;
+    var max = params.max === undefined ? (min + 5) : params.max;
+
+    var sliderMin = params.defaultMin === undefined? min : params.defaultMin;
+    var sliderMax = params.defaultMax === undefined? max : params.defaultMax;
+
+    var bbox = d3.select(this.id).node().getBoundingClientRect();
+
+    var height = bbox.height;
+    var width = bbox.width;
+
+    var sliderScale = d3.scale.linear().domain([min, max]).range([ this.margin.left, width - this.margin.right ]);
+
+    var axis = d3.svg.axis().scale(sliderScale).orient("bottom");
+
+
+    var line = d3.select(this.id)
+    .append("rect")
+    .attr("x", this.margin.left)
+    .attr("y", sliderObj.margin.top)
+    .attr("width", width - this.margin.right - this.margin.left)
+    .attr("height", height/2)
+    .attr("fill", "white");
+
+    var slider = d3.select(this.id).append("g");
+
+    var gAxis = slider.append("g").attr("class", "axis").attr("transform", "translate(0,"+(sliderObj.margin.top + height/2)+")").call(axis);
+
+    var sliderRect = slider.append("rect")
+    .attr("x", sliderScale(sliderMin))
+    .attr("y", sliderObj.margin.top)
+    .attr("width", sliderWidth())
+    .attr("height", height/2)
+    .attr("fill", "red");
+
+    var sliderLeftHandle = slider.append("rect")
+    .attr("x", sliderScale(sliderMin))
+    .attr("y", sliderObj.margin.top)
+    .attr("width", 10)
+    .attr("height", height/2)
+    .attr("fill", "black");
+
+    var leftDrag = d3.behavior.drag();
+    leftDrag.on("drag", function(){
+        var currentX = +sliderLeftHandle.attr("x");
+        var newX = currentX + d3.event.dx;
+        if (sliderScale.invert(newX) < min) {
+            sliderMin = min;
+        } else {
+            sliderMin = sliderScale.invert(newX );
+            callChangeEventListeners();
+        }
+        redraw();
+    })
+    sliderLeftHandle.call(leftDrag);
+
+    var sliderRightHandle = slider.append("rect")
+    .attr("x", sliderScale(sliderMax) - 10)
+    .attr("y", sliderObj.margin.top)
+    .attr("width", 10)
+    .attr("height", height/2)
+    .attr("fill", "black");
+
+    var rightDrag = d3.behavior.drag();
+    rightDrag.on("drag", function(){
+        var currentX = +sliderRightHandle.attr("x");
+
+        var newX = currentX + d3.event.dx + 10;
+
+        if (sliderScale.invert(newX) > max) {
+            sliderMax = max;
+        } else {
+            sliderMax = sliderScale.invert(newX);
+            callChangeEventListeners();
+        }
+        redraw();
+    })
+    sliderRightHandle.call(rightDrag);
+
+
+    var sliderRectDrag = d3.behavior.drag();
+    sliderRectDrag.on("drag", function(){
+        var currentLeftX = +sliderLeftHandle.attr("x");
+        var newLeftX = currentLeftX + d3.event.dx;
+
+        var currentRightX = +sliderRightHandle.attr("x");
+        var newRightX = currentRightX + d3.event.dx + 10;
+
+        if (sliderScale.invert(newLeftX) >= min && sliderScale.invert(newRightX) <= max) {
+            sliderMin = sliderScale.invert(newLeftX);
+            sliderMax = sliderScale.invert(newRightX);
+            callChangeEventListeners();
+        }
+        redraw();
+    })
+    sliderRect.call(sliderRectDrag);
+
+    function redraw(){
+        sliderRightHandle.attr("x", sliderScale(sliderMax) - 10);
+        sliderLeftHandle.attr("x", sliderScale(sliderMin));
+        sliderRect.attr("x", sliderScale(sliderMin)).attr("width", sliderWidth());
+    }
+
+    function sliderWidth(){
+        return sliderScale(sliderMax) - sliderScale(sliderMin);
+    }
+
+    this.getSliderMin = function() {
+        return sliderMin;
+    }
+
+    this.getSliderMax = function() {
+        return sliderMax;
+    }
+
+    var changeEventListeners = [];
+    this.on = function(event, fn) {
+        switch (event) {
+            case "change":
+                if( fn === null) {
+                    changeEventListeners = [];
+                }else {
+                    changeEventListeners.push(fn);
+                }
+                break;
+        }
+    }
+
+    function callChangeEventListeners(){
+        for(var i in changeEventListeners){
+            changeEventListeners[i]();
+        }
+    }
+}
